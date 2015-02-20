add7a0b018a260af7da7b2fa61c21a60a0d6bbab
Kabir Khan <kkhan@redhat.com>
2015-02-20 15:36:57 +0100

    Make possible to use non-snapshot versions, and to copy sources in one go

--- spi/dependencies/src/main/resources/update-resources.sh
+++ spi/dependencies/src/main/resources/update-resources.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# To update, checkout the wildfly-core tag to update to and build it.
+# Then run this script to update the sources, passing in the wildfly-core
+# checkout root folder as the argument
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$DIR/../../../lib
@@ -8,11 +12,11 @@ echo Updating jars from $1 to $DIR
 
 echo
    
-rm $DIR/*-SNAPSHOT.jar
+rm $DIR/*.jar
 
-cp -v $1/controller/target/wildfly-controller-*-SNAPSHOT.jar $DIR
-cp -v $1/core-model-test/framework/target/wildfly-core-model-test-framework-*-SNAPSHOT.jar $DIR
-cp -v $1/deployment-repository/target/wildfly-deployment-repository-*-SNAPSHOT.jar $DIR
-cp -v $1/model-test/target/wildfly-model-test-*-SNAPSHOT.jar $DIR
-cp -v $1/server/target/wildfly-server-*-SNAPSHOT.jar $DIR
-cp -v $1/subsystem-test/framework/target/wildfly-subsystem-test-framework-*-SNAPSHOT.jar $DIR
+cp -v $1/controller/target/wildfly-controller-*.jar $DIR
+cp -v $1/core-model-test/framework/target/wildfly-core-model-test-framework-*.jar $DIR
+cp -v $1/deployment-repository/target/wildfly-deployment-repository-*.jar $DIR
+cp -v $1/model-test/target/wildfly-model-test-*.jar $DIR
+cp -v $1/server/target/wildfly-server-*.jar $DIR
+cp -v $1/subsystem-test/framework/target/wildfly-subsystem-test-framework-*.jar $DIR
