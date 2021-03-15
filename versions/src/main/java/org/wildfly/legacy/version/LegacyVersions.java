036dec6ba1442426bc9fda7ae2d96c55f4409f9f
Yeray Borges <yborgess@redhat.com>
2021-03-15 16:35:06 +0000

    [WFCORE-5346] Add core and subsystem modules for WF23
    - DMR files have been built based on WildFly Core 15.0.0.Final and WildFly 23.0.0.Final

--- versions/src/main/java/org/wildfly/legacy/version/LegacyVersions.java
+++ versions/src/main/java/org/wildfly/legacy/version/LegacyVersions.java
@@ -52,6 +52,7 @@ public class LegacyVersions {
     }
 
     public static void main(String[] args) {
+        output("wf23");
         output("7.3.0");
         output("7.2.0");
         output("7.0.0");
